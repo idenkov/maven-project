52089812f4c325b07d8cad25b36db3be6dfe759b
idenkov <ivan.denkov87@gmail.com>
2017-07-22 04:47:52 +0300

    checkstyle

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
-  
+
   /**
    * @param someone The name of the person
    * @return greeting string
    */
-  public String greet(final String someone) {
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
